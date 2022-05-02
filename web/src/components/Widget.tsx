541dd4e45e9f76fa3633045aa31d98a9f8e56bae
Diego Imperiano <imperiano.cn@gmail.com>
2022-05-02 16:47:56 -0300

    Primeiro commit | Criação do projeto e finalização da perimeira aula

---
+++ web/src/components/Widget.tsx
@@ -0,0 +1,21 @@
+import { ChatTeardropDots } from 'phosphor-react'
+
+import { Popover } from '@headlessui/react'
+
+export function Widget(){
+
+  return (
+    <Popover className='absolute bottom-5 right-5'>
+      <Popover.Panel>hello world</Popover.Panel> 
+
+      <Popover.Button className='flex items-center h-12 px-3 text-white rounded-full bg-brand-500 group'>
+        <ChatTeardropDots  className='w-6 h-6'/>
+
+        <span className='overflow-hidden transition-all duration-500 ease-linear max-w-0 group-hover:max-w-xs'>
+          <span className='pl-2'></span> 
+          Feedback
+        </span>
+      </Popover.Button>
+    </Popover>
+  )
+}
